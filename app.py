e9568533849f0749a2467999972a5ce89b5cf76e
Atabak-Alishiri <alishiriatabak@gmail.com>
2024-04-05 13:28:26 -0700

    debug mode is off now

--- app.py
+++ app.py
@@ -488,4 +488,4 @@ def update_minimum_wage_trend(selected_country, selected_year):
 
 # Run the app
 if __name__ == "__main__":
-    app.run_server(debug=True, host="127.0.0.1")
+    app.run_server(debug=False, host="127.0.0.1")
