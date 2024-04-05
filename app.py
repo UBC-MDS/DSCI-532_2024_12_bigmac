5c6e71039ae6c22e0aca957e22d1cb37f80d3bfa
Atabak Alishiri <143530834+atabak-alishiri@users.noreply.github.com>
2024-04-05 13:37:01 -0700

    42 deploying the apppy (#43)
    * debug mode is off now
    
    * app.py is ready for deployment
    
    * requirements file added

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ df = data_wrangling.main()
 
 # Initialize the Dash app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = app.server
 
 # Define the figure for the global map view
 fig_map = px.choropleth(
@@ -488,4 +489,4 @@ def update_minimum_wage_trend(selected_country, selected_year):
 
 # Run the app
 if __name__ == "__main__":
-    app.run_server(debug=True, host="127.0.0.1")
+    app.run_server(debug=False, host="127.0.0.1")
