8e16dfb1d782cb5f4fb948000efa02566bddcea0
Atabak Alishiri <143530834+atabak-alishiri@users.noreply.github.com>
2024-04-12 17:16:48 -0700

    update: fixed labels and legends (#74)
    * added labeles to axis
    
    * removed legend from median hourly bar chart
    
    * removed x-axis label on barchart

--- src/app.py
+++ src/app.py
@@ -23,54 +23,56 @@ server = app.server
 
 # Text field
 
+
 def year_slider():
     return dbc.Container(
-            [
-                html.H6("Year Range"),
-                html.Div(
-                    dcc.RangeSlider(
-                        id="year-slider",
-                        min=df["year"].min(),
-                        max=df["year"].max(),
-                        value=[
-                            df["year"].min(),
-                            df["year"].max(),
-                        ],
-                        # marks={str(year): str(year) for year in df["year"].unique()},
-                        marks={
-                            (str(year)): (str(year) if year % 3 == 1 else " ")
-                            for year in df["year"].unique()
-                        },
-                        step=None,
-                    )
-                ),
-            ],
-            style={
-                "margin-left": "-4rem",
-                # "margin-right": "2rem",
-                "padding": "2rem 1rem",
-            },
+        [
+            html.H6("Year Range"),
+            html.Div(
+                dcc.RangeSlider(
+                    id="year-slider",
+                    min=df["year"].min(),
+                    max=df["year"].max(),
+                    value=[
+                        df["year"].min(),
+                        df["year"].max(),
+                    ],
+                    # marks={str(year): str(year) for year in df["year"].unique()},
+                    marks={
+                        (str(year)): (str(year) if year % 3 == 1 else " ")
+                        for year in df["year"].unique()
+                    },
+                    step=None,
+                )
+            ),
+        ],
+        style={
+            "margin-left": "-4rem",
+            # "margin-right": "2rem",
+            "padding": "2rem 1rem",
+        },
     )
+
 
 def country_dropdown():
     return dbc.Container(
-            [
-                html.H6("Country"),
-                html.Div(
-                    dcc.Dropdown(
-                        id="country-dropdown",
-                        options=[
-                            {"label": i, "value": i} for i in df["country"].unique()
-                        ],
-                        value="United States",
-                    ),
+        [
+            html.H6("Country"),
+            html.Div(
+                dcc.Dropdown(
+                    id="country-dropdown",
+                    options=[
+                        {"label": i, "value": i} for i in df["country"].unique()
+                    ],
+                    value="United States",
                 ),
-            ],
-            style={
-                "margin-left": "-6rem",
-                "margin-right": "7rem",
-                "padding": "2rem 1rem",
-            },
+            ),
+        ],
+        style={
+            "margin-left": "-6rem",
+            "margin-right": "7rem",
+            "padding": "2rem 1rem",
+        },
     )
 
 
@@ -102,6 +104,7 @@ def inflation_control():
             "padding": "2rem 0rem",
         },
     )
+
 
 def currency_control():
     return html.Div(
@@ -148,17 +151,17 @@ def key_metrics():
                     ],
                 )
             ],
-            
+
         ),
         color="light",
         outline=True,
         style={
-                "height":850,
-                "margin-left": "5rem",
-                "margin-right": "1rem",
-                "margin-bottom": "2rem",
-                # "padding": "2rem 1rem",
-            },
+            "height": 850,
+            "margin-left": "5rem",
+            "margin-right": "1rem",
+            "margin-bottom": "2rem",
+            # "padding": "2rem 1rem",
+        },
     )
 
 
@@ -166,20 +169,20 @@ def global_map():
     # Define the figure for the global map view
     return dbc.Card(
         dbc.CardBody(
-            [   html.Br(), 
+            [html.Br(),
                 dvc.Vega(id='global-map', spec={})
-                ],
-            
+             ],
+
         ),
         color="light",
         outline=True,
         style={
-                "height":850,
-                # "margin-left": "1rem",
-                "margin-right": "5rem",
-                "margin-bottom": "2rem",
-                # "padding": "2rem 1rem",
-            },
+            "height": 850,
+            # "margin-left": "1rem",
+            "margin-right": "5rem",
+            "margin-bottom": "2rem",
+            # "padding": "2rem 1rem",
+        },
     )
 
 
@@ -198,15 +201,15 @@ def time_series_plot():
                     # style={"display": "inline-block"},
                 ),
             ],
-            
+
         ),
         color="light",
         outline=True,
         style={
-                "margin-left": "5rem",
-                "margin-right": "1rem",
-                "margin-bottom": "2rem",
-            },
+            "margin-left": "5rem",
+            "margin-right": "1rem",
+            "margin-bottom": "2rem",
+        },
     )
 
 
@@ -222,10 +225,10 @@ def minimum_wage_trend_plot():
         color="light",
         outline=True,
         style={
-                "margin-left": "1rem",
-                "margin-right": "5rem",
-                "margin-bottom": "2rem",
-            },
+            "margin-left": "1rem",
+            "margin-right": "5rem",
+            "margin-bottom": "2rem",
+        },
     )
 
 
@@ -249,14 +252,14 @@ def footer():
         ],
         style={
             "width": "100%",
-            "bottom":"0",
+            "bottom": "0",
             "text-align": "center",
             "margin-top": "14px",
             "margin-bottom": "0px",
             "font-size": "13px",
             "opacity": "0.6",
             "background-color": "#EBEBEB",
-            "color":"#2a3f5f",
+            "color": "#2a3f5f",
         },
     )
 
@@ -272,29 +275,29 @@ app.layout = html.Div(
                             dbc.Col(
                                 html.Div(
                                     [
-                                                    dbc.Container(
-                                                        [
-                                                            html.H2(
-                                                                "Big Mac Index Dashboard",
-                                                                style={
-                                                                        "textAlign": "center", 
-                                                                       "color": "#2a3f5f",                                                                       },
-                                                            ),
-                                                            html.P(
-                                                                """
+                                        dbc.Container(
+                                            [
+                                                html.H2(
+                                                    "Big Mac Index Dashboard",
+                                                    style={
+                                                        "textAlign": "center",
+                                                        "color": "#2a3f5f", },
+                                                ),
+                                                html.P(
+                                                    """
                                                                 Inflation measurements within this dashboard derive from variations in Big Mac prices, rather than the Consumer Price Index (CPI). 
                                                                 The concept of buying power is visualized through 'Big Macs per hour'—the number of Big Macs one can purchase with an hour's wage. 
                                                                 This unique approach offers an insightful perspective on economic conditions across different countries.
                                                                 """,
-                                                                style={
-                                                                    'marginTop': '10px', 
-                                                                    'textAlign': 'center',
-                                                                     "color": "#2a3f5f"
-                                                                    }  # Ensuring the text is centered and has some space above
-                                                            ),
-                                                            html.Hr()
-                                                        ],
-                                                    )
+                                                    style={
+                                                        'marginTop': '10px',
+                                                        'textAlign': 'center',
+                                                        "color": "#2a3f5f"
+                                                    }  # Ensuring the text is centered and has some space above
+                                                ),
+                                                html.Hr()
+                                            ],
+                                        )
                                     ]
                                 )
                             )
@@ -344,6 +347,7 @@ app.layout = html.Div(
     ]
 )
 
+
 @app.callback(
     Output("global-map", "spec"),
     [Input("year-slider", "value"), Input("country-dropdown", "value")]
@@ -359,12 +363,13 @@ def update_global_map(selected_year, selected_country):
 
     df_map = gdf[['iso3', 'geometry']].merge(filtered_data, right_on='country_code', left_on='iso3', how='left'
                                              ).rename({'bigmacs_per_hour': 'Big Macs per Hour'}, axis=1)
-    
+
     background = alt.Chart(df_map).mark_geoshape(color="lightgrey")
     chart_map = background + alt.Chart(df_map, width=600, height=600).mark_geoshape().encode(
-            color=alt.Color('Big Macs per Hour', legend=alt.Legend(orient='bottom-right')),
-            tooltip=['country', 'Big Macs per Hour']
-        ).properties(height=600)
+        color=alt.Color('Big Macs per Hour',
+                        legend=alt.Legend(orient='bottom-right')),
+        tooltip=['country', 'Big Macs per Hour']
+    ).properties(height=600)
 
     highlight = chart_map + alt.Chart(df_map).mark_geoshape(
         fill=None,
@@ -373,9 +378,8 @@ def update_global_map(selected_year, selected_country):
     ).transform_filter(
         alt.FieldEqualPredicate(field='country', equal=selected_country)
     )
-    
-    return (highlight).to_dict(format="vega")
 
+    return (highlight).to_dict(format="vega")
 
 
 @app.callback(
@@ -436,7 +440,8 @@ def update_minimum_wage_trend(selected_country, selected_year):
         fig.add_trace(
             go.Bar(
                 x=[country],
-                y=filtered_data[filtered_data["country"] == country]["Wage in USD"],
+                y=filtered_data[filtered_data["country"]
+                                == country]["Wage in USD"],
                 name=country,
                 marker_color="lightslategray"
                 if country != selected_country
@@ -447,17 +452,19 @@ def update_minimum_wage_trend(selected_country, selected_year):
     # Update the layout
     fig.update_layout(
         title=f"Median Hourly Wage from {selected_year[0]} to {selected_year[1]}",
-        xaxis_title="Country",
         yaxis_title="Hourly Wage (USD)",
         xaxis={"categoryorder": "total descending"},
         # margin={"l": 40, "b": 40, "t": 40, "r": 0},
         hovermode="closest",
         height=400,
+        showlegend=False
     )
 
     return fig
 
 # Callback to update the time series plot
+
+
 @app.callback(
     Output("time-series-plot", "figure"),
     [
@@ -477,31 +484,38 @@ def update_time_series(selected_country, selected_year, inflation, currency):
     if (inflation == "adjust") & (currency == "local"):
         wage = "Adjusted Wage in Local Currency"
         y_data = "Bigmac Price in Local Currency"
+        y_label = "Amount in Local Currency"
 
     elif (inflation == "adjust") & (currency == "USD"):
         wage = "Adjusted Wage in USD"
         y_data = "Bigmac Price in USD"
+        y_label = "Amount in USD"
 
     elif (inflation == "absolute") & (currency == "local"):
         wage = "Wage in Local Currency"
         y_data = "Bigmac Price in Local Currency"
+        y_label = "Amount in Local Currency"
 
-    else:  #  (inflation == "absolute") & (currency == "USD")
+    else:  # (inflation == "absolute") & (currency == "USD")
         wage = "Wage in USD"
         y_data = "Bigmac Price in USD"
+        y_label = "Amount in USD"
 
     fig = px.line(
         filtered_data,
         x="year",
         y=[y_data, wage],
         labels={
-                "year": "Year",
-                 },
+            "year": "Year",
+            y_data: y_label,
+            wage: y_label
+        },
         title=f"Big Mac Price and Minimum Wage Trends in {selected_country}",
-    ).update_yaxes(visible=False)
+    )
+
+    fig.update_yaxes(visible=True, title_text=y_label)
 
     return fig
-
 
 
 # Run the app
