52482e6b52a8fd69b418b4329e0c49a424b6c4e2
Atabak-Alishiri <alishiriatabak@gmail.com>
2024-04-12 17:18:52 -0700

    changed the debug mode

--- src/app.py
+++ src/app.py
@@ -506,4 +506,4 @@ def update_time_series(selected_country, selected_year, inflation, currency):
 
 # Run the app
 if __name__ == "__main__":
-    app.run_server(debug=True, host="127.0.0.1")
+    app.run_server(debug=False, host="127.0.0.1")
